8c48ee1e9f274904bf1fe683e37ee87ee8400b8d
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-02-19 15:38:40 +0530

    Time: 0 ms (100.00%), Space: 12.1 MB (48.40%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -14,51 +14,35 @@ public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         
         vector<vector<int>> ans;
-        
-        if(root==NULL)
-        {
-            return ans;
-        }
+        if(root==NULL)  return ans; 
+        queue<TreeNode* > q;
+        q.push(root);
         bool flag=false;
-        queue<TreeNode * > q;
-        
-    q.push(root);
         
         while(q.empty()==false)
         {
-            
-           
             int size=q.size();
-          
-            vector<int> level_nodes;
-          
-            for(int i=0;i<size;i++){
-                 TreeNode* curr= q.front();
-                  q.pop();
-                level_nodes.push_back(curr->val);
-                if(curr->left!=NULL)
-                {
-                    q.push(curr->left);
-                }
-                if(curr->right!=NULL)
-                {
-                    q.push(curr->right);
-                }
-            }
+            vector<int> level;
             
+            for(int i=0;i<size;i++)
+            {
+                auto curr=q.front();
+                q.pop();
+                level.push_back(curr->val);
+                if(curr->left!=NULL) q.push(curr->left);
+                if(curr->right!=NULL) q.push(curr->right);
+            }
             if(flag==true)
             {
-                reverse(level_nodes.begin(),level_nodes.end());
-                ans.push_back(level_nodes);
+                reverse(level.begin(),level.end());
+                flag=false;
             }
-            else{
-                ans.push_back(level_nodes);
+            else
+            {
+                flag=true;
             }
-            flag=!flag;
-            
+            ans.push_back(level);
         }
-        
         return ans; 
-        
     }
 };
