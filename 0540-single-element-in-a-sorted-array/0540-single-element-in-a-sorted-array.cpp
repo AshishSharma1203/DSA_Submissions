ed1f669fefd8f0387ff97f1350fa19a1d2e0f16a
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-02-21 22:55:14 +0530

    Time: 32 ms (43.92%), Space: 22.4 MB (62.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,6 +2,10 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
        
+        
+        // ##    Note:our array size is always odd 
+         // same elements index will be in pair like (0,1) (2,3) .. indexed elements would be same ideally 
+        
        // brute force 
         // our array size is always odd 
 //         int ans=-1;
@@ -23,16 +27,28 @@ public:
         while(low<high)
         {
          int mid=(low+high)/2;
+            
+            // check if mid is 0 or n-1 then this is our answer
             if(mid==0 || mid==n-1)  return nums[mid];
-           if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
-            if(mid%2==0 && nums[mid]==nums[mid-1]){
+            
+            // check if any of the  element before and after mid arent equal then it is ans
+            
+           if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
+               return nums[mid];
+            
+            // if index of mid is even then ideally there are even elements in left part and being in pair they      would be  idential ,hence current mid element should be different 
+            
+            if(mid%2==0 && nums[mid]==nums[mid-1])
                 high=mid-1;
-            }
+            
+            // if index of mid is odd then ideally there are odd elements in left part and being in pair they      would be  idential hence current mid element should match with previous one 
              
            else  if(mid%2!=0 && nums[mid]!=nums[mid-1])
             {
                 high=mid-1;
             }
+            
+            // if both conditions fail then there is fault in right side of array 
             else 
                 low=mid+1;
             
