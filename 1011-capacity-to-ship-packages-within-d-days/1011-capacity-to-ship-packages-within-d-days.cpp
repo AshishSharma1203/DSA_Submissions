9862e0405b6ff7f87fbef3294119b7a8bb3c89fd
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-02-23 02:52:19 +0530

    Time: 97 ms (12.38%), Space: 46.5 MB (5.77%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,69 +1,73 @@
-class Solution {
-public:
+class Solution
+{
+    public:
     
-    int solve(vector<int> weights)
-    {
-        int sum=0;
-        for(int i=0;i<weights.size();i++)
-            sum+=weights[i];
-        
-        return sum; 
-    }
+// find overall weight of all the packages 
     
-    int check(vector<int>weights,int mid,int days)
-    {
-        int life=0;
-        int currwt=0;
-        
-        for(int i=0;i<weights.size();i++)
+        int solve(vector<int> weights)
         {
-            if(weights[i]>mid)  return -1;
-          if(currwt+weights[i]>mid)
-          {
-              life++;
-              currwt=weights[i];
-          }
-            else {
-                currwt+=weights[i];
-            }
+            int sum = 0;
+            for (int i = 0; i < weights.size(); i++)
+                sum += weights[i];
+
+            return sum;
         }
-        life++;
-        if(life>days)  return -1;
-        return life;
-    }
-    
-    int shipWithinDays(vector<int>& weights, int days) {
-        
-        int n=weights.size();
-        
-        int ans=INT_MAX;
-        int sum=solve(weights);
-        
-        int low=weights[0],high=sum;
-        
-        while(low<=high)
+
+    int check(vector<int> weights, int mid, int days)
+    {
+        int life = 0, currwt = 0;
+
+        for (int i = 0; i < weights.size(); i++)
         {
-            int mid=(low+high)/2;
-            
-            int curr=check(weights,mid,days);
-            // if(curr!=-1)  ans=min(ans,mid);
-            // if(curr==-1)
-            // {
-            //     low=mid+1;
-            // }
-            // cout<<mid<<" "<<curr<<endl;
-            if(curr!=-1)
+            if (weights[i] > mid) return -1;
+            if (currwt + weights[i] > mid)
             {
-                ans=min(ans,mid);
-                high=mid-1;
+                life++;
+                currwt = weights[i];
             }
             else
             {
-                low=mid+1;
+                currwt += weights[i];
             }
         }
+        life++;
+        if (life > days) return -1;
+        return life;
+    }
+
+    int shipWithinDays(vector<int> &weights, int days)
+    {
+
+        int n = weights.size(), ans = INT_MAX;
+
+        int sum = solve(weights);	// find the overall sum of all the  packages.
+
+       	// soo  min wt= lowest weights of available package and max wt. = overall sum of all packages .ie. sum 
+
+        int low = weights[0], high = sum;
+
+        // apply binary search here
         
-        return ans; 
-        
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;	// mid is current min capacity of ship 
+
+            int curr = check(weights, mid, days);	// check the days required to ship with this particular value               of ship capacity 
+
+            if (curr != -1)
+            {
+               	// it means this is accepted value for min. value of ship
+
+                ans = min(ans, mid);	// this is our possible answer 
+                high = mid - 1;	// go to left side to check if any optimum min . vlaue exists or not  
+            }
+            else
+            {
+               	// it means it is not possible to carry all packages in given days with curr min capacity of ship
+                low = mid + 1;
+            }
+        }
+
+        return ans;
     }
 };
