d12110a4a939fbeac0e851d9c47f18ab82cd46e7
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-03-15 20:27:27 +0530

    Time: 11 ms (10.92%), Space: 10.6 MB (10.56%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -1,49 +1,49 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    bool isCompleteTree(TreeNode* root) {
-        
-        // perform level order traversal and check whether all nodes are present or not
-        
-        if(root==NULL ||(root->left==NULL && root->right==NULL))  return true;
-        
-        queue<TreeNode*>q;
-        q.push(root);
-        
-        while(q.empty()==false)
-        {
-           TreeNode* curr=q.front();
-            q.pop();
-            if(curr==NULL) break;
-            else
-            {
-                 q.push(curr->left);
-                q.push(curr->right);
-            }
-        }
-        
-        // check if there is still any non NULL node present in the queue  
-        
-        while(q.empty()==false)
+class Solution
+{
+    public:
+        bool isCompleteTree(TreeNode *root)
         {
 
-        TreeNode*curr=q.front();
-            q.pop();
-            if(curr!=NULL) return false;
-            
+           	// perform level order traversal and check whether all nodes are present or not
+
+            if (root == NULL || (root->left == NULL && root->right == NULL)) return true;
+
+            queue<TreeNode*> q;
+            q.push(root);
+
+            while (q.empty() == false)
+            {
+                TreeNode *curr = q.front();
+                q.pop();
+                if (curr == NULL) break;
+                else
+                {
+                    q.push(curr->left);
+                    q.push(curr->right);
+                }
+            }
+
+           	// check if there is still any non NULL node present in the queue (is such soo then it indicates that all nodes are not in left)  
+
+            while (q.empty() == false)
+            {
+
+                TreeNode *curr = q.front();
+                q.pop();
+                if (curr != NULL) return false;
+            }
+
+            return true;
         }
-        
-        return true; 
-        
-    }
 };
