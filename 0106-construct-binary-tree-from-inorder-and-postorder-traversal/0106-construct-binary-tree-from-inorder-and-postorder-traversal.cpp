7b531272b459a7881cfbc4548d86231250a36c2b
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-03-17 01:02:40 +0530

    Time: 12 ms (92.40%), Space: 26.5 MB (25.72%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,49 +1,50 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    
-    
-    
-    TreeNode* solve(vector<int>&inorder,vector<int>&postorder,int inStart,int inEnd,int postStart,int postEnd, unordered_map<int,int> &mp)
-    {
-        if(inStart>inEnd || postStart>postEnd)  return NULL;
-        
-        TreeNode* root=new TreeNode(postorder[postEnd]);
-        int index=mp[root->val];
-        int elements=inEnd-index;
-        root->left=solve(inorder,postorder,inStart,index-1,postStart,postEnd-elements-1,mp);
-        root->right=solve(inorder,postorder,index+1,inEnd,postEnd-elements,postEnd-1,mp);
-        
-        return root;
-    }
-    
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        
-       
-         unordered_map<int,int> mp;
-        int n=inorder.size(),m=postorder.size();
-        
-        for(int i=0;i<n;i++)
+class Solution
+{
+    public:
+
+        TreeNode* solve(vector<int> &inorder, vector<int> &postorder, int inStart, int inEnd, int postStart, int postEnd, unordered_map< int, int> &mp)
         {
-            
-             mp.insert({inorder[i],i});
+            if (inStart > inEnd || postStart > postEnd) return NULL;
+
+            TreeNode *root = new TreeNode(postorder[postEnd]);
+            int index = mp[root->val];
+            int elements = inEnd - index;
+           	// no of elements in left will be equal to index-inStart  
+            root->left = solve(inorder, postorder, inStart, index - 1, postStart, postEnd - elements - 1, mp);
+           	// no of elements in right will be inEnd-index 
+            root->right = solve(inorder, postorder, index + 1, inEnd, postEnd - elements, postEnd - 1, mp);
+
+            return root;
         }
-        
-        int inStart=0,inEnd=n-1,postStart=0,postEnd=m-1;
-        
-        TreeNode*root= solve(inorder,postorder,inStart,inEnd,postStart,postEnd,mp);
-        
+
+    TreeNode* buildTree(vector<int> &inorder, vector<int> &postorder)
+    {
+
+        int n = inorder.size(), m = postorder.size();
+        unordered_map<int, int> mp;
+       	// store the index and elements of postorder traversal
+        for (int i = 0; i < n; i++)
+        {
+
+            mp.insert({ inorder[i],i });
+                
+        }
+
+        int inStart = 0, inEnd = n - 1, postStart = 0, postEnd = m - 1;
+
+        TreeNode *root = solve(inorder, postorder, inStart, inEnd, postStart, postEnd, mp);
+
         return root;
-        
     }
 };
