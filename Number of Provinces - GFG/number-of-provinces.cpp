72923d279574d290e8d3e78d4f9b33ae123c28e4
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-04-13 16:58:37 +0530

    Added solution - LeetHub

--- Number of Provinces - GFG/number-of-provinces.cpp
+++ Number of Provinces - GFG/number-of-provinces.cpp
@@ -6,50 +6,158 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
+class DisjointSet
+{
+  
+  public:
+  vector<int> parent,size;
+  
+  DisjointSet(int n)
+  {
+      parent.resize(n);
+      size.resize(n);
+      
+      for(int i=0;i<n;i++)
+      {
+          parent[i]=i;
+          size[i]=1;
+      }
+  }
+  
+  int findUparent(int node)
+  {
+      if(parent[node]==node)
+      return node;
+      
+      return parent[node] = findUparent(parent[node]);
+  }
+  
+//   void UnionBySize(int u,int v)
+//   {
+//       int par_u=findUparent(u),par_v=findUparent(v);
+//       if(par_u==par_v) return ;
+      
+//       if(size[par_u]>size[par_v])
+//       {
+//           par_v=parent[par_u];
+//           size[par_u]+=size[par_v];
+//       }
+//       else 
+//       {
+//           par_u=parent[par_v];
+//           size[par_v]+=size[par_u];
+//       }
+//   }
+    void unionBySize(int u, int v) {
+        int ulp_u = findUparent(u);
+        int ulp_v = findUparent(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+  
+};
+
 class Solution {
   public:
   
-   void bfs(int u, vector<vector < int>> &isConnected, vector< bool > &visited)
-        {
-
-            queue<int> q;
-            q.push(u);
-
-            while (q.empty() == false)
-            {
-
-                int curr = q.front();
-                q.pop();
-                visited[curr] = true;
-
-                for (int i = 0; i < isConnected[curr].size(); i++)
-                {
-                    if (isConnected[curr][i] == 1 && visited[i] == false)
-                    {
-                        q.push(i);
-                    }
-                }
-            }
-        }
   
-    int numProvinces(vector<vector<int>> adj, int V) {
-        // code here
+//   using bfs 
+//   void bfs(int u, vector<vector < int>> &isConnected, vector< bool > &visited)
+//         {
+
+//             queue<int> q;
+//             q.push(u);
+
+//             while (q.empty() == false)
+//             {
+
+//                 int curr = q.front();
+//                 q.pop();
+//                 visited[curr] = true;
+
+//                 for (int i = 0; i < isConnected[curr].size(); i++)
+//                 {
+//                     if (isConnected[curr][i] == 1 && visited[i] == false)
+//                     {
+//                         q.push(i);
+//                     }
+//                 }
+//             }
+//         }
+  
+  
+//   using dfs 
+  
+//   void dfs ( int i, vector<vector<int>> &adj, vector<bool> &visited)
+//   {
+       
+//       visited[i]==true;
+       
+//       for(int j=0;j<adj[i].size();j++)
+//       {
+//           if(adj[i][j]==1 && visited[j]==false)
+//           {
+//               visited[j]=true;
+//                 dfs(j,adj,visited);
+//           }
+          
+//       }
+       
+//   }
+  
+//     int numProvinces(vector<vector<int>> adj, int V) {
+//         // code here
         
-        int ans = 0;
+//         int ans = 0;
       
 
-        vector<bool> visited(V, false);
+//         vector<bool> visited(V, false);
 
-        for (int i = 0; i < V; i++)
-        {
-            if (visited[i] == false)
-            {
-                bfs(i, adj, visited);
-                ans++;
-            }
-        }
-        return ans;
+//         for (int i = 0; i < V; i++)
+//         {
+//             if (visited[i] == false)
+//             {
+//                  ans++;
+//                 // bfs(i, adj, visited);
+//                 dfs(i, adj, visited);
+               
+//             }
+//         }
+//         return ans;
     
+//     }
+
+    int numProvinces(vector<vector<int>> adj, int V) {
+        // code here
+      
+      DisjointSet ds(V);
+      for(int i=0;i<V;i++)
+      {
+          for(int j=0;j<V;j++)
+          {
+              if(i!=j && adj[i][j]==1)
+              {
+                  ds.unionBySize(i,j);
+              }
+          }
+      }
+      
+      int ans=0;
+      for(int i=0;i<V;i++)
+      {
+          if(ds.parent[i]==i)
+          ans++;
+      }
+    
+    
+    return ans; 
     }
 };
 
