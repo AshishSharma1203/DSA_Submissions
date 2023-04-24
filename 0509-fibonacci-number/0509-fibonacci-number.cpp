0d42a77f5818388f4630922121468f3d94459d7f
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-04-24 17:58:24 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (10.07%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    
 //     Recursive solution
     
 //     int fib(int n) {
@@ -14,24 +15,49 @@ public:
     
     // memoization approach 
     
-    int  solve(int n, vector<int> & dp)
-    {
+//     int  solve(int n, vector<int> & dp)
+//     {
  
-        if(n==0 || n==1 )
-        {
-           return dp[n]=n;
-        }
-        if(dp[n]!=-1)
-            return dp[n];
+//         if(n==0 || n==1 )
+//         {
+//            return dp[n]=n;
+//         }
+//         if(dp[n]!=-1)
+//             return dp[n];
         
-        return dp[n]= solve(n-1,dp)+ solve(n-2,dp);
-    }
+//         return dp[n]= solve(n-1,dp)+ solve(n-2,dp);
+//     }
+    
+//     int fib(int n) {
+        
+//       vector<int> dp(n+1,-1);
+//        return  solve(n,dp);
+
+//     }
+    
+    
+    // tabulation approach  (bottom up approach)
+    
     
     int fib(int n) {
         
-      vector<int> dp(n+1,-1);
-       return  solve(n,dp);
+        if(n<=1) return n;
+        vector<int> dp(n+1, -1);
+        
+        dp[0]=0 ;
+        dp[1]=1;
+        
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        
+        return dp[n];
+         
+  
 
+ 
+        
     }
     
 };
