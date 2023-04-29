01a913b11b04342513b66d8e9b403e56b05f748e
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-04-29 15:45:21 +0530

    Time: 5 ms (14.62%), Space: 6.5 MB (56.47%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -18,7 +18,8 @@ public:
 //     }
     
     
-        
+    // memoization approach     
+    
     int solve(int i,int j,int m,int n, vector<vector<int>> &dp)
     {
         
@@ -40,9 +41,29 @@ public:
     
     int uniquePaths(int m, int n) {
           
-        vector<vector<int>> dp(m, vector<int> (n,0));
+        // vector<vector<int>> dp(m, vector<int> (n,0));
         
-        return solve(0,0,m,n,dp);
+        // return solve(0,0,m,n,dp);
+        
+        // tabulation approach 
+        
+        vector<vector<int>> dp(m,vector<int> (n,0));
+        
+        for(int i=0;i<m;i++)
+            dp[i][0]=1;
+        for(int j=0;j<n;j++)
+            dp[0][j]=1;
+        
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+              // dp[i][j]= sum of ways to reach its adjacent left cel + up cell 
+                dp[i][j]= dp[i][j-1] + dp[i-1][j];
+            }
+        }
+        
+        return dp[m-1][n-1];
         
     }
 };
