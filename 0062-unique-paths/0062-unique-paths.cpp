2ab09682205f69667475f15fb73de8e0bebce9ae
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-04-29 15:45:30 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (34.11%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -20,23 +20,23 @@ public:
     
     // memoization approach     
     
-    int solve(int i,int j,int m,int n, vector<vector<int>> &dp)
-    {
+//     int solve(int i,int j,int m,int n, vector<vector<int>> &dp)
+//     {
         
-        if(i==m-1 && j==n-1 )
-        {
-            return 1;
-        }
-        if(i>=m || j>= n)
-            return 0;
+//         if(i==m-1 && j==n-1 )
+//         {
+//             return 1;
+//         }
+//         if(i>=m || j>= n)
+//             return 0;
         
-        if(dp[i][j]!=0)
-            return dp[i][j];
-          int down= solve(i+1,j, m,n,dp);
-        int right= solve(i,j+1,m,n,dp);
+//         if(dp[i][j]!=0)
+//             return dp[i][j];
+//           int down= solve(i+1,j, m,n,dp);
+//         int right= solve(i,j+1,m,n,dp);
         
-        return dp[i][j]= down+right;
-    }
+//         return dp[i][j]= down+right;
+//     }
     
     
     int uniquePaths(int m, int n) {
