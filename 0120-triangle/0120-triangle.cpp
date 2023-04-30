c6102302071408a2bba3852bb125e096f614b512
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-05-01 01:07:22 +0530

    Time: 4 ms (81.81%), Space: 8.9 MB (58.15%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     
+    // memoization approach 
+    
     int solve(int i,int j,  int n,vector<vector<int>>& triangle, vector<vector<int>> &dp)
     {
 
@@ -21,8 +23,39 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         
         int n=triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return solve(0,0,n, triangle,dp);
+        // vector<vector<int>> dp(n,vector<int>(n,-1));
+        // return solve(0,0,n, triangle,dp);
+        
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        dp[0][0]= triangle[0][0];
+        
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                // if(j>i)
+                //     continue;
+                
+                if(j==0)
+                    dp[i][j]= triangle[i][j]+ dp[i-1][j];
+                
+                else if(j==i)
+                    dp[i][j]=triangle[i][j]+ dp[i-1][j-1];
+                else 
+                {
+                    dp[i][j]= triangle[i][j] + min(dp[i-1][j] , dp[i-1][j-1]);
+                }
+                
+            }
+        }
+        
+        int ans= INT_MAX;
+        
+        for(int i=0;i<n;i++)
+            ans= min(ans,dp[n-1][i]);
+        
+        return ans; 
         
     }
 };
+
