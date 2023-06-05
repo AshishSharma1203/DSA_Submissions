4de843c1726faa17bd2baec76ad3873179f87e7a
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-06-05 23:37:32 +0530

    Time: 28 ms (49.03%), Space: 12.3 MB (42.88%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,32 +1,32 @@
-class Solution {
-public:
-    int minDistance(string word1, string word2) {
-        
-        int n= word1.length(), m= word2.length();
-        
-        // find the LCS of both the strings  
-        
-        // using tabulation method  
-        
-        vector<vector<int>> dp (n+1, vector<int> ( m+1, 0));
-        
-        for(int i=1;i<=n; i++)
+class Solution
+{
+    public:
+        int minDistance(string word1, string word2)
         {
-            for(int j=1;j<= m; j++)
+
+            int n = word1.length(), m = word2.length();
+
+           	// find the LCS of both the strings  
+
+           	// using tabulation method  
+
+            vector<vector < int>> dp(n + 1, vector<int> (m + 1, 0));
+
+            for (int i = 1; i <= n; i++)
             {
-                
-                if(word1[i-1]==word2[j-1])
-                    dp[i][j]= 1+ dp[i-1][j-1];
-                else
-                    dp[i][j]= max(dp[i-1][j], dp[i][j-1]);
-                
-                
+                for (int j = 1; j <= m; j++)
+                {
+
+                    if (word1[i - 1] == word2[j - 1])
+                        dp[i][j] = 1 + dp[i - 1][j - 1];
+                    else
+                        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
             }
+           	// cout<<dp[n][m];
+           	// so required ans is sum of length of both strings - 2* LCS 
+            int ans = n + m - (2 *dp[n][m]);
+
+            return ans;
         }
-        // cout<<dp[n][m];
-        int ans = n+m -(2* dp[n][m]);
-        
-        return ans; 
-        
-    }
 };
