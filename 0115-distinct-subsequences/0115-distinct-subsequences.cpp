485fced9a109cd24e9c8e45cd057c93a485a5d03
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-06-07 00:40:56 +0530

    Time: 2623 ms (11.63%), Space: 422.6 MB (9.65%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -5,9 +5,8 @@ class Solution
        	// Recursive approach 
         int solve(int i, int j, string s, string t)
         {
-
-            if (i < 0) return 0;
             if (j < 0) return 1;
+            if (i < 0) return 0;
 
             if (s[i] == t[j])
                 return solve(i - 1, j - 1, s, t) + solve(i - 1, j, s, t);
@@ -21,14 +20,15 @@ class Solution
     {
         if (j < 0) return 1;
         if (i < 0) return 0;
-        
 
         if (dp[i][j] != -1) return dp[i][j];
 
         if (s[i] == t[j])
+           	// take the current char at i in s, or we can try to search on left part of s string 
             return dp[i][j] = mem_solve(i - 1, j - 1, s, t, dp) + mem_solve(i - 1, j, s, t, dp);
 
         else
+           	// here we need to reduce the i pointer to match the strng t with s 
             return dp[i][j] = mem_solve(i - 1, j, s, t, dp);
     }
 
