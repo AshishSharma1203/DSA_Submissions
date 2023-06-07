eba8e2732ea330d5e533373e0564dd82b24386af
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-06-07 10:36:01 +0530

    Time: 57 ms (42.96%), Space: 18.5 MB (67.51%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,18 +1,18 @@
 class Solution
 {
     public:
+        int mod = 1e9 + 7;
+   	// Recursive approach 
+    int solve(int i, int j, string s, string t)
+    {
+        if (j < 0) return 1;
+        if (i < 0) return 0;
 
-       	// Recursive approach 
-        int solve(int i, int j, string s, string t)
-        {
-            if (j < 0) return 1;
-            if (i < 0) return 0;
-
-            if (s[i] == t[j])
-                return solve(i - 1, j - 1, s, t) + solve(i - 1, j, s, t);
-            else
-                return solve(i - 1, j, s, t);
-        }
+        if (s[i] == t[j])
+            return solve(i - 1, j - 1, s, t) + solve(i - 1, j, s, t);
+        else
+            return solve(i - 1, j, s, t);
+    }
 
    	// Memoization approach
 
@@ -25,7 +25,7 @@ class Solution
 
         if (s[i] == t[j])
            	// take the current char at i in s, or we can try to search on left part of s string 
-            return dp[i][j] = mem_solve(i - 1, j - 1, s, t, dp) + mem_solve(i - 1, j, s, t, dp);
+            return dp[i][j] = (mem_solve(i - 1, j - 1, s, t, dp) + mem_solve(i - 1, j, s, t, dp)) % mod;
 
         else
            	// here we need to reduce the i pointer to match the strng t with s 
@@ -36,7 +36,31 @@ class Solution
     {
 
         int n = s.length(), m = t.length();
-        vector<vector < int>> dp(n, vector<int> (m, -1));
-        return mem_solve(n - 1, m - 1, s, t, dp);
+       	// vector<vector < int>> dp(n, vector<int> (m, -1));
+       	// return mem_solve(n - 1, m - 1, s, t, dp);
+
+       	// Tabulation appraoch
+
+        vector<vector < int>> dp(n + 1, vector<int> (m + 1, 0));
+
+       	// convert base cases 
+       	// if(j < 0) return 1
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+
+                if (s[i - 1] == t[j - 1])
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j])% mod;
+
+                else
+                    dp[i][j] = dp[i - 1][j]% mod;;
+            }
+        }
+
+        return dp[n][m];
     }
 };
