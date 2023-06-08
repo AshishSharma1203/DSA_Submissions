6c0d7c1ec370bb4042a3479e39b5a9ef3e51e66a
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-06-08 23:45:30 +0530

    Time: 800 ms (30.53%), Space: 217.8 MB (23.45%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -31,7 +31,7 @@ class Solution
 
    	// memoization approach  
 
-    int mem_solve(int i, bool buy, int count, vector<int> &prices,  vector<vector<vector<int>>> &dp)
+    int mem_solve(int i, bool buy, int count, vector<int> &prices, vector<vector< vector< int>>> &dp)
     {
 
         if (i == prices.size() || count == 0)
@@ -69,10 +69,58 @@ class Solution
 
         int count = 2;
         bool buy = false;
+       	// recursive 
+       	// vector<vector < int>> dp(n, vector<int> (2, -1));
 
-        // vector<vector < int>> dp(n, vector<int> (2, -1));
-        vector<vector<vector<int>>> dp(n,vector<vector<int>> (2, vector<int> (3,-1)));
-
+        vector<vector<vector< int>>> dp(n, vector<vector < int>> (2, vector<int> (3, -1)));	// (index,buy,count)
+       	// memoization 
         return mem_solve(0, buy, count, prices, dp);
+
+       	// tabulation approach 
+
+        for (int i = 0; i <= 2; i++)
+        {
+            dp[n][0][i] = 0;
+            dp[n][1][i] = 0;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= 1; j++)
+            {
+                dp[i][j][0] = 0;
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int buy = 0; buy <= 1; buy++)
+            {
+                for (int count = 1; count <= 2; count++)
+                {
+
+                    int profit;
+
+                    if (buy == false)
+                    {
+
+                        int buy = -prices[i] +  dp[i+1][1][count];
+                        int not_buy = 0 + dp[i+1][0][count];
+                        profit = max(buy, not_buy);
+                    }
+                    else if (buy == true)
+                    {
+
+                        int retain = 0 + dp[i+1][1][count];
+                        int sell = prices[i] + dp[i+1][0][count-1] ;
+                        profit = max(retain, sell);
+                    }
+
+                     dp[i][buy][count] = profit;
+                }
+            }
+        }
+        
+        return dp[0][0][count];
+        
     }
 };
