9353cb3e0f14ffc66e283e73622717ae122645a9
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-06-08 20:58:06 +0530

    Time: 19 ms (9.50%), Space: 15.2 MB (21.23%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,65 +4,65 @@ class Solution
 
        	// Recursive approach 
 
-        int solve(int i, bool bought, vector<int> &prices)
+//         int solve(int i, bool bought, vector<int> &prices)
 
-    {
+//     {
 
-        if (i == prices.size()) return 0;
+//         if (i == prices.size()) return 0;
 
-        int profit = 0;
+//         int profit = 0;
 
-        if (bought == false)
-        {
+//         if (bought == false)
+//         {
 
-            int buy = -prices[i] + solve(i + 1, true, prices);
-            int not_buy = solve(i + 1, false, prices);
+//             int buy = -prices[i] + solve(i + 1, true, prices);
+//             int not_buy = solve(i + 1, false, prices);
 
-            profit = max(buy, not_buy);
-        }
-        else if (bought == true)
-        {
-            int retain = 0 + solve(i + 1, true, prices);
-            int sell = prices[i] + solve(i + 1, false, prices);
-            profit = max(retain, sell);
-        }
+//             profit = max(buy, not_buy);
+//         }
+//         else if (bought == true)
+//         {
+//             int retain = 0 + solve(i + 1, true, prices);
+//             int sell = prices[i] + solve(i + 1, false, prices);
+//             profit = max(retain, sell);
+//         }
 
-        return profit;
-    }
+//         return profit;
+//     }
 
    	// memoized approach 
 
-    int mem_solve(int i, bool bought, vector<int> &prices, vector<vector< int>> &dp)
-    {
+//     int mem_solve(int i, bool bought, vector<int> &prices, vector<vector< int>> &dp)
+//     {
 
-        if (i == prices.size()) return 0;
+//         if (i == prices.size()) return 0;
 
-        if (dp[i][bought] != -1) return dp[i][bought];
+//         if (dp[i][bought] != -1) return dp[i][bought];
 
-        int profit = 0;
+//         int profit = 0;
 
-        if (bought == false)
-        {
+//         if (bought == false)
+//         {
 
-            int buy = -prices[i] + solve(i + 1, true, prices);
-            int not_buy = solve(i + 1, false, prices);
+//             int buy = -prices[i] + solve(i + 1, true, prices);
+//             int not_buy = solve(i + 1, false, prices);
 
-            profit = max(buy, not_buy);
-        }
-        else if (bought == true)
-        {
-            int retain = 0 + solve(i + 1, true, prices);
-            int sell = prices[i] + solve(i + 1, false, prices);
-            profit = max(retain, sell);
-        }
+//             profit = max(buy, not_buy);
+//         }
+//         else if (bought == true)
+//         {
+//             int retain = 0 + solve(i + 1, true, prices);
+//             int sell = prices[i] + solve(i + 1, false, prices);
+//             profit = max(retain, sell);
+//         }
 
-        return dp[i][bought] = profit;
-    }
+//         return dp[i][bought] = profit;
+//     }
 
     int maxProfit(vector<int> &prices)
     {
 
-        // bool bought = false;
+       	// bool bought = false;
        	// return solve(0, bought, prices);
 
        	// memoization solution
@@ -72,7 +72,7 @@ class Solution
 
        	// tabulation approach 
 
-        vector<vector < int>> dp(n+1, vector<int> (2, -1));
+        vector<vector < int>> dp(n + 1, vector<int> (2, -1));
         dp[n][0] = dp[n][1] = 0;
 
         for (int i = n - 1; i >= 0; i--)
@@ -81,7 +81,7 @@ class Solution
             for (int bought = 0; bought <= 1; bought++)
             {
 
-                int profit ;
+                int profit;
 
                 if (bought == 0)
                 {
@@ -98,10 +98,11 @@ class Solution
                     profit = max(retain, sell);
                 }
 
-               dp[i][bought] = profit;
+                dp[i][bought] = profit;
             }
         }
 
+        // our ans will be 
         return dp[0][0];
     }
 };
