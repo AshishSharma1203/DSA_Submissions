7e833d4fd00aea793b9040d08280f4aace255db5
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-06-09 21:53:09 +0530

    Time: 2017 ms (5.01%), Space: 292.4 MB (7.63%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,46 @@
+class Solution
+{
+    public:
+       	// Recursive approach 
+        int solve(int i, int last, vector<int> &nums)
+        {
+
+            if (i == nums.size()) return 0;
+
+            int take = 0;
+            if (last == -1 || nums[i] > nums[last])
+            {
+                take = 1 + solve(i + 1, i, nums);
+            }
+
+            int nontake = 0 + solve(i + 1, last, nums);
+
+            return max(take, nontake);
+        }
+
+    int mem_solve(int i, int last, vector<int> &nums, vector<vector< int>> &dp)
+    {
+        if (i == nums.size())
+            return 0;
+        if (dp[i][last + 1] != -1)
+            return dp[i][last + 1];
+
+        int take = 0;
+        if (last == -1 || nums[i] > nums[last])
+        {
+            take = 1 + mem_solve(i + 1, i, nums, dp);
+        }
+
+        int nontake = 0 + mem_solve(i + 1, last, nums, dp);
+
+        return dp[i][last + 1] = max(take, nontake);
+    }
+
+    int lengthOfLIS(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<vector < int>> dp(n, vector<int> (n + 1, -1));
+
+        return mem_solve(0, -1, nums, dp);
+    }
+};
