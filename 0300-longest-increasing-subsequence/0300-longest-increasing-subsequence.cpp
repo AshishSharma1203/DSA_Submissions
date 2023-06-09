e2898edaf3542d51ad72516194c7e76f4942bc4a
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-06-09 21:53:30 +0530

    Time: 1841 ms (5.01%), Space: 292.2 MB (14.86%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,39 +2,39 @@ class Solution
 {
     public:
        	// Recursive approach 
-        int solve(int i, int last, vector<int> &nums)
-        {
+       	//         int solve(int i, int last, vector<int> &nums)
+       	//         {
 
-            if (i == nums.size()) return 0;
+       	//             if (i == nums.size()) return 0;
+
+       	//             int take = 0;
+       	//             if (last == -1 || nums[i] > nums[last])
+       	//             {
+       	//                 take = 1 + solve(i + 1, i, nums);
+       	//             }
+
+       	//             int nontake = 0 + solve(i + 1, last, nums);
+
+       	//             return max(take, nontake);
+       	//         }
+
+        int mem_solve(int i, int last, vector<int> &nums, vector<vector< int>> &dp)
+        {
+            if (i == nums.size())
+                return 0;
+            if (dp[i][last + 1] != -1)
+                return dp[i][last + 1];
 
             int take = 0;
             if (last == -1 || nums[i] > nums[last])
             {
-                take = 1 + solve(i + 1, i, nums);
+                take = 1 + mem_solve(i + 1, i, nums, dp);
             }
 
-            int nontake = 0 + solve(i + 1, last, nums);
+            int nontake = 0 + mem_solve(i + 1, last, nums, dp);
 
-            return max(take, nontake);
+            return dp[i][last + 1] = max(take, nontake);
         }
-
-    int mem_solve(int i, int last, vector<int> &nums, vector<vector< int>> &dp)
-    {
-        if (i == nums.size())
-            return 0;
-        if (dp[i][last + 1] != -1)
-            return dp[i][last + 1];
-
-        int take = 0;
-        if (last == -1 || nums[i] > nums[last])
-        {
-            take = 1 + mem_solve(i + 1, i, nums, dp);
-        }
-
-        int nontake = 0 + mem_solve(i + 1, last, nums, dp);
-
-        return dp[i][last + 1] = max(take, nontake);
-    }
 
     int lengthOfLIS(vector<int> &nums)
     {
