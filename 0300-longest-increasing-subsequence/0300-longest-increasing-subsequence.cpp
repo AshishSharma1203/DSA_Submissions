df3113854d790e35bc2d66090ae512647f72e600
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-06-10 10:51:28 +0530

    Time: 762 ms (25.12%), Space: 292.2 MB (14.88%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -18,6 +18,7 @@ class Solution
        	//             return max(take, nontake);
        	//         }
 
+       	// memoizatoin approach 
         int mem_solve(int i, int last, vector<int> &nums, vector<vector< int>> &dp)
         {
             if (i == nums.size())
@@ -39,8 +40,33 @@ class Solution
     int lengthOfLIS(vector<int> &nums)
     {
         int n = nums.size();
-        vector<vector < int>> dp(n, vector<int> (n + 1, -1));
+       	//         vector<vector < int>> dp(n, vector<int> (n + 1, -1));
 
-        return mem_solve(0, -1, nums, dp);
+       	//         return mem_solve(0, -1, nums, dp);
+
+       	// Tabulation approach 
+
+        vector<vector < int>> dp(n+1, vector<int> (n + 1, 0));
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+
+            for (int last = i - 1; last >= -1; last--)
+            {
+                int take = 0;
+                if (last == -1 || nums[i] > nums[last])
+                {
+                   	// take = 1 + mem_solve(i + 1, i, nums, dp);
+                    take = 1 + dp[i + 1][i+1];
+                }
+
+               	// int nontake = 0 + mem_solve(i + 1, last, nums, dp);
+                int nontake = 0 + dp[i + 1][last+1];
+
+                dp[i][last + 1] = max(take, nontake);
+            }
+        }
+
+        return dp[0][0];
     }
 };
