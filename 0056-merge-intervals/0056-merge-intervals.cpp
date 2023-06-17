7dd231ef9ab258bfeafc72de05c494d4fda42a9c
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-06-17 11:20:22 +0530

    Time: 50 ms (24.07%), Space: 18.9 MB (78.45%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,36 +1,36 @@
-class Solution {
-public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        
-        int n= intervals.size();
-        vector<vector<int>>ans;
-        sort(intervals.begin(),intervals.end());
-        
-        ans.push_back(intervals[0]);
-        
-        for(int i=1;i<n;i++)
+class Solution
+{
+    public:
+        vector<vector < int>> merge(vector<vector < int>> &intervals)
         {
-            
-            int curr_size= ans.size();
-            int prev_start= ans[curr_size-1][0];
-            int prev_end= ans[curr_size-1][1];
-            int curr_start=intervals[i][0];
-            int curr_end=intervals[i][1];
-            
-            if(curr_start<=prev_end)
+
+            int n = intervals.size();
+            vector<vector < int>> ans;
+            sort(intervals.begin(), intervals.end());
+
+            ans.push_back(intervals[0]);
+
+            for (int i = 1; i < n; i++)
             {
 
-                ans[curr_size-1][1]= max(curr_end, prev_end);
-                
+                int curr_size = ans.size();
+                int prev_start = ans[curr_size - 1][0];
+                int prev_end = ans[curr_size - 1][1];
+                int curr_start = intervals[i][0];
+                int curr_end = intervals[i][1];
+
+               	// check if curr_start is less than last inteval's end time  
+                if (curr_start <= prev_end)
+                {
+
+                    ans[curr_size - 1][1] = max(curr_end, prev_end);
+                }
+                else
+                {
+                    ans.push_back(intervals[i]);
+                }
             }
-            else
-            {
-                ans.push_back(intervals[i]);
-            }
-            
+
+            return ans;
         }
-        
-        return ans;
-        
-    }
 };
