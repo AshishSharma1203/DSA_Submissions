eea3dbecc15829420274120b236303d49d3bd367
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-06-23 11:28:33 +0530

    Time: 3 ms (95.59%), Space: 8.3 MB (27.59%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,32 +1,50 @@
 /**
  *Definition for singly-linked list.
  *struct ListNode {
- *    int val;
- *    ListNode * next;
- *    ListNode() : val(0), next(nullptr) {}
- *    ListNode(int x) : val(x), next(nullptr) {}
- *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *   int val;
+ *   ListNode * next;
+ *   ListNode() : val(0), next(nullptr) {}
+ *   ListNode(int x) : val(x), next(nullptr) {}
+ *   ListNode(int x, ListNode *next) : val(x), next(next) {}
  *};
  */
 class Solution
 {
     public:
-        ListNode* reverseList(ListNode *head)
+
+        ListNode* reverseListRecursive(ListNode *curr, ListNode *prev)
         {
 
-            if (head == NULL || head->next == NULL) return head;
+            if (curr == NULL)
+                return prev;
 
-            ListNode *curr = head, *prev = NULL;
+            ListNode *curr_next = curr->next;
+            curr->next = prev;
 
-            while (curr != NULL)
-            {
-
-                ListNode *curr_next = curr->next;
-                curr->next = prev;
-                prev = curr;
-                curr = curr_next;
-            }
-
-            return prev;
+            return reverseListRecursive(curr_next, curr);
         }
+
+    ListNode* reverseList(ListNode *head)
+    {
+
+        if (head == NULL || head->next == NULL) return head;
+
+        ListNode *curr = head, *prev = NULL;
+
+       	// recursive code
+
+        return reverseListRecursive(curr, prev);
+
+       	//            	// Iterative code  
+       	//             while (curr != NULL)
+       	//             {
+
+       	//                 ListNode *curr_next = curr->next;
+       	//                 curr->next = prev;
+       	//                 prev = curr;
+       	//                 curr = curr_next;
+       	//             }
+
+       	//             return prev;
+    }
 };
