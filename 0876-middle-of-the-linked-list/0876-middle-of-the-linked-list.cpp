eb43eca0b99ae895d66729cafe00f2978e3cc00f
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-06-23 12:21:59 +0530

    Time: 5 ms (36.24%), Space: 7 MB (94.13%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -1,37 +1,53 @@
 /**
  *Definition for singly-linked list.
  *struct ListNode {
- *    int val;
- *    ListNode * next;
- *    ListNode() : val(0), next(nullptr) {}
- *    ListNode(int x) : val(x), next(nullptr) {}
- *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *   int val;
+ *   ListNode * next;
+ *   ListNode() : val(0), next(nullptr) {}
+ *   ListNode(int x) : val(x), next(nullptr) {}
+ *   ListNode(int x, ListNode *next) : val(x), next(next) {}
  *};
  */
 class Solution
 {
     public:
+
         ListNode* middleNode(ListNode *head)
         {
 
            	// approach 1: count the node and then return middle  
 
-            if (head == NULL || head->next == NULL) return head;
-            int count = 0;
+           	//             if (head == NULL || head->next == NULL) return head;
+           	//             int count = 0;
 
-            for (ListNode *curr = head; curr != NULL; curr = curr->next)
+           	//             for (ListNode *curr = head; curr != NULL; curr = curr->next)
+           	//             {
+           	//                 count++;
+           	//             }
+           	//             count = (int) count / 2 + 1;
+
+           	//             ListNode *curr = head;
+           	//             while (count != 1)
+           	//             {
+           	//                 curr = curr->next;
+           	//                 count--;
+           	//             }
+
+           	//             return curr;
+
+           	// approach 2: using slow and fast pointers  
+            
+            if(head==NULL || head->next==NULL) return head;
+            
+            ListNode* slow=head, *fast= head;
+            
+            while(fast!=NULL && fast->next!=NULL)
             {
-                count++;
+                fast=fast->next->next;
+                slow=slow->next;
             }
-            count = (int) count / 2 + 1;
-
-            ListNode *curr = head;
-            while (count != 1)
-            {
-                curr = curr->next;
-                count--;
-            }
-
-            return curr;
+            
+            return slow;
+            
         }
 };
