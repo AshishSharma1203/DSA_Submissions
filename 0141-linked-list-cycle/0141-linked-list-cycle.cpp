00016364a5403cb4477b6d2f24b02e4f7f561d45
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-06-24 22:57:03 +0530

    Time: 7 ms (96.57%), Space: 8.1 MB (35.67%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -1,9 +1,9 @@
 /**
  *Definition for singly-linked list.
  *struct ListNode {
- *    int val;
- *    ListNode * next;
- *    ListNode(int x) : val(x), next(NULL) {}
+ *int val;
+ *ListNode * next;
+ *ListNode(int x) : val(x), next(NULL) {}
  *};
  */
 class Solution
@@ -11,18 +11,35 @@ class Solution
     public:
         bool hasCycle(ListNode *head)
         {
-            unordered_set<ListNode*> st;
+
+           	// approach 1: modify the links and use a boolean attribute 
+           	// approach 2: use a temp node and point nodes to it while traversing 
+           	// Approach 3: use map;
+           	//             unordered_set<ListNode*> st;
+           	//             if (head == NULL || head->next == NULL) return false;
+           	//             ListNode *curr = head;
+
+           	//             while (curr != NULL)
+           	//             {
+
+           	//                 if (st.count(curr))
+           	//                     return true;
+           	//                 st.insert(curr);
+           	//                 curr = curr->next;
+           	//             }
+           	//             return false;
+
+           	// approach 4: using floyd algo of cycle detetction 
             if (head == NULL || head->next == NULL) return false;
-            ListNode *curr = head;
+            ListNode *slow = head, *fast = head;
 
-            while (curr != NULL)
+            while (fast != NULL && fast->next != NULL)
             {
-
-                if (st.count(curr))
-                    return true;
-                st.insert(curr);
-                curr = curr->next;
+                slow = slow->next;
+                fast = fast->next->next;
+                if (slow == fast) return true;
             }
+
             return false;
         }
 };
