8ca3ed294da759310fa15b9259b9194c0e61b65a
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-07-03 19:44:38 +0530

    Time: 26 ms (80.65%), Space: 22.3 MB (33.83%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,59 +1,64 @@
-class Solution {
-public:
-    int singleNonDuplicate(vector<int>& nums) {
-       
-        
-        // ##    Note:our array size is always odd 
-         // same elements index will be in pair like (0,1) (2,3) .. indexed elements would be same ideally 
-        
-       // brute force 
-        // our array size is always odd 
-//         int ans=-1;
-//         if(nums.size()==1)  return nums[0];
-        
-//         for(int i=1;i<nums.size();i=i+2)
-//         {
-//             if(nums[i]!=nums[i-1])  
-//                 return nums[i-1];
-//         }
-//         return nums[nums.size()-1];
-        
-        int n=nums.size();
-        int low=0,high=n-1;
-        int ans=-1;
-        
-        if(n==1)  return nums[0];
-        
-        while(low<high)
+class Solution
+{
+    public:
+        int singleNonDuplicate(vector<int> &nums)
         {
-         int mid=(low+high)/2;
-            
-            // check if mid is 0 or n-1 then this is our answer
-            if(mid==0 || mid==n-1)  return nums[mid];
-            
-            // check if any of the  element before and after mid arent equal then it is ans
-            
-           if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
-               return nums[mid];
-            
-            // if index of mid is even then ideally there are even elements in left part and being in pair they      would be  idential ,hence current mid element should be different 
-            
-            if(mid%2==0 && nums[mid]==nums[mid-1])
-                high=mid-1;
-            
-            // if index of mid is odd then ideally there are odd elements in left part and being in pair they      would be  idential hence current mid element should match with previous one 
-             
-           else  if(mid%2!=0 && nums[mid]!=nums[mid-1])
+
+           	// Approach 1: using xor operator
+
+           	//int numxor = nums[0];
+           	// for (int i = 1; i < nums.size(); i++)
+           	// {
+           	//     numxor = numxor ^ nums[i];
+           	// }
+           	// return numxor;
+
+           	// approach : use linear search 
+
+           	//             for(int i=0;i < nums.size()-1;i=i+2)
+           	//             {
+           	//                 if(nums[i]!=nums[i+1])
+           	//                     return nums[i];
+           	//             }
+           	//             return nums[nums.size()-1];
+
+           	// approach 3 : use binary search  
+
+            int n = nums.size();
+            int low = 0, high = n - 1;
+            int ans = -1;
+
+            if (n == 1) return nums[0];
+
+            while (low < high)
             {
-                high=mid-1;
+                int mid = (low + high) / 2;
+
+               	// check if mid is 0 or n-1 then this is our answer
+               	if (mid == 0 || mid == n - 1) return nums[mid];
+
+               	// check if any of the  element before and after mid arent equal then it is ans
+
+                if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+                    return nums[mid];
+
+               	// if index of mid is even then ideally there are even elements in left part and being in pair they      would be  idential ,hence current mid element should be different 
+
+                if (mid % 2 == 0 && nums[mid] == nums[mid - 1])
+                    high = mid - 1;
+
+               	// if index of mid is odd then ideally there are odd elements in left part and being in pair they      would be  idential hence current mid element should match with previous one 
+
+                else if (mid % 2 != 0 && nums[mid] != nums[mid - 1])
+                {
+                    high = mid - 1;
+                }
+
+               	// if both conditions fail then there is fault in right side of array 
+                else
+                    low = mid + 1;
             }
-            
-            // if both conditions fail then there is fault in right side of array 
-            else 
-                low=mid+1;
-            
+            return nums[low];
+            return nums[low];
         }
-        return nums[low];
-        
-    }
 };
