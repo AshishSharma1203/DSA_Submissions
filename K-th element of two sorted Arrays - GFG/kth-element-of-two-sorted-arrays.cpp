bf79c9dbdc5f40765c0cac83e49fb9d6d099805f
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-07-04 20:50:15 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -8,52 +8,38 @@ class Solution{
     public:
     int kthElement(int arr1[], int arr2[], int n, int m, int k)
     {
-        // approach 1: use temp array and sort and return temp[k-1]
         
-        // approach 2: use two pointer 
-        int i=0,j=0, count=0, ans=0;
         
-        while(i<n && j<m)
+        if(n>m)
+        return kthElement(arr2,arr1,m,n,k);
+        
+        int low=max(0,k-m), high=min(n,k);
+        
+        while(low<=high)
         {
-            if(arr1[i]<arr2[j])
+            int mid= (low+high)/2;
+            int cut1=mid;
+            int cut2=k-cut1;
+            
+            int l1=(cut1==0)?INT_MIN:arr1[cut1-1];
+            int l2=(cut2==0)?INT_MIN:arr2[cut2-1];
+            int r1=(cut1==n)?INT_MAX:arr1[cut1];
+            int r2=(cut2==m)?INT_MAX:arr2[cut2];
+            
+            if(l1<=r2 && l2<=r1)
             {
-               
-                count++;
-                ans=arr1[i];
-                 i++;
+                return max(l1,l2);
+            }
+            else if(l1>r2)
+            
+            {
+                high=mid-1;
             }
             else 
-            {
-               
-                count++;
-                ans=arr2[j];
-                 j++;
-            }
+            low=mid+1;
             
-            if(count==k)
-            return ans;
-            
-        }
-        
-        while(i<n)
-        {
-            count++;
-            if(count==k)
-            return arr1[i];
-            i++;
-        }
-        while(j<m)
-        {
-            count++;
-            if(count==k)
-            {
-                return arr2[j];
-            }
-            j++;
         }
         return -1;
-        
-        
         
     }
 };
