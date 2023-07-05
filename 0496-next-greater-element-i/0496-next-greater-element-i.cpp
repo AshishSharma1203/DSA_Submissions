115e5643cb16fcf3de525cbabef5eb0bf33e2ae8
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-07-05 20:53:12 +0530

    Time: 7 ms (63.86%), Space: 8.9 MB (56.28%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,37 +1,72 @@
 class Solution
 {
     public:
+
+       	// approach 1: use linear traversal 
+       	//     {
+       	//      int n = nums1.size(), m = nums2.size();
+       	//      unordered_map<int, int> mp;
+       	//             for (int i = 0; i < m; i++)
+       	//             {
+
+       	//                 for (int j = i + 1; j < m; j++)
+       	//                 {
+
+       	//                     if (nums2[j] > nums2[i])
+       	//                     {
+       	//                         mp.insert({ nums2[i],
+       	//                             nums2[j] });
+       	//                         break;
+       	//                     }
+       	//                 }
+       	//             }
+
+       	//            vector<int> ans;
+       	//             for (int i = 0; i < n; i++)
+       	//             {
+
+       	//                 if (mp.find(nums1[i]) != mp.end())
+       	//                 {
+       	//                     ans.push_back(mp[nums1[i]]);
+       	//                 }
+       	//                 else
+       	//                     ans.push_back(-1);
+       	//             }
+
+       	//             return ans;
+       	//     }
+
         vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
         {
             int n = nums1.size(), m = nums2.size();
             unordered_map<int, int> mp;
-            for (int i = 0; i < m; i++)
+           	// to store element and its greater element of nums2  array  
+
+            stack<int> st;
+            mp.insert({ nums2[m - 1],
+                -1 });
+
+            st.push(nums2[m - 1]);
+            for (int i = m - 2; i >= 0; i--)
             {
 
-                for (int j = i + 1; j < m; j++)
-                {
+                int curr = nums2[i];
 
-                    if (nums2[j] > nums2[i])
-                    {
-                        mp.insert({ nums2[i],
-                            nums2[j] });
-                        break;
-                    }
+                while (!st.empty() && st.top() < curr)
+                {
+                    st.pop();
                 }
+                int next_greater = st.empty() ? -1 : st.top();
+                mp.insert({ nums2[i],
+                    next_greater });
+                st.push(nums2[i]);
             }
 
-           vector<int> ans;
+            vector<int> ans;
             for (int i = 0; i < n; i++)
             {
-
-                if (mp.find(nums1[i]) != mp.end())
-                {
-                    ans.push_back(mp[nums1[i]]);
-                }
-                else
-                    ans.push_back(-1);
+                ans.push_back(mp[nums1[i]]);
             }
-
             return ans;
         }
 };
