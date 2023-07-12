b8aaf318ac98c07e7958588982409b758d4f6f00
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-07-12 20:32:59 +0530

    Time: 32 ms (39.93%), Space: 28.6 MB (7.73%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -1,48 +1,50 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    
-    int solve(TreeNode* root,int &ans)
-    {
-        if(root==NULL)
+class Solution
+{
+    public:
+
+        int solve(TreeNode *root, int &ans)
         {
-            return 0;
+            if (root == NULL)
+            {
+                return 0;
+            }
+
+            int lmax = solve(root->left, ans);	// left side max value 
+            int rmax = solve(root->right, ans);	//right siide max value
+
+           	//         possible paths are : stop at currnode, currnode+left path, currnode+rightpath, currnode+left path+ currnode+ right path 
+
+            int lmx_root = max(root->val, (root->val + lmax));	//leftmax+root-value  path
+            int rmx_root = max(root->val, (root->val + rmax));	//rightmax+root-value path
+            int overall_sum = root->val + lmax + rmax;	// leftmax+rightmax+root->val path
+
+           	// now update ans with  max possible value of path 
+
+            // ans = max(ans, max(root->val, max(overall_sum, max(lmx_root, rmx_root))));
+                ans=max({ans,root->val,overall_sum,lmx_root,rmx_root});
+
+           	// now return max of root node only  or rootnode +any left or side path 
+
+            return max(root->val, root->val + max(lmax, rmax));
         }
-        
-        int lmax=solve(root->left,ans); // left side max value 
-        int rmax=solve(root->right,ans); //right siide max value
-        
-        int lmx_root=max(root->val,(root->val+lmax)); //leftmax+root-value  path
-        int rmx_root=max(root->val,(root->val+rmax)); //rightmax+root-value path
-        int overall_sum=root->val+lmax+rmax;  // leftmax+rightmax+root->val path
-        
-        
-        // now update ans with  max possible value of path 
-        
-        ans= max(ans,max(root->val,max(overall_sum,max(lmx_root,rmx_root))));
-        
-        // now return max of root node only  or rootnode +any left or side path 
-        
-        return max(root->val,root->val+max(lmax,rmax));
-        
-    }
-    
-    int maxPathSum(TreeNode* root) {
-       int ans= INT_MIN;
-      
-        solve(root,ans);
-        
+
+    int maxPathSum(TreeNode *root)
+    {
+        int ans = INT_MIN;
+
+        solve(root, ans);
+
         return ans;
-        
     }
 };
