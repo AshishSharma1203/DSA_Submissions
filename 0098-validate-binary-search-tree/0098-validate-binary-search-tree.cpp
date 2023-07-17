48e26a49f2f4451d440d00d5e6348755adc12bed
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-07-17 23:14:28 +0530

    Time: 13 ms (65.60%), Space: 21.7 MB (58.36%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,37 +1,47 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
+class Solution
+{
+    public:
     
-    bool solve(TreeNode* root,long low,long high)
+    bool solve(TreeNode* curr,long min ,long max)
     {
-        if(root==NULL) return true;
+
+        if(curr==NULL)
+            return true;
+        
+        // if(curr->val>= max && curr->val<=max)
+        //     return false;
+        
+        return (curr->val<max && curr->val>min )&& solve(curr->left,min,curr->val)&& solve(curr->right,curr->val,max);
     
-    // if(root->left==NULL && root->right==NULL)
-    //     return true;
-    
-        
-        bool left_flag=solve(root->left,low,root->val);
-        bool right_flag=solve(root->right,root->val,high);
-        
-        return left_flag && right_flag && (root->val>low && root->val<high);
-        
     }
     
-    bool isValidBST(TreeNode* root) {
-     long low=LONG_MIN,high=LONG_MAX;
-    
-        bool ans=solve(root,low,high);
-        
-        return ans; 
-    }
+        bool isValidBST(TreeNode *root)
+        {
+
+           	// approach 1; at each node find the max  element of left and min  elemenet of right if(curr_val < min ||curr_val>max) returnn false else recursively check for other nodes 
+
+            
+            // approach 2; form inorder and then whether its increaesing or noot 
+            
+            // approach 3:
+            
+            if(root==NULL || (root->left==NULL &&root->right==NULL))
+                return true;
+            
+            long  min=LONG_MIN , max=LONG_MAX;
+            
+            return solve(root,min,max);
+            
+        }
 };
