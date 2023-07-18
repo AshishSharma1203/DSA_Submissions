44369ccb7478bf3688e8a443b0e084bcfdde55ee
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-07-18 20:15:25 +0530

    Time: 15 ms (91.08%), Space: 24 MB (68.02%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -1,41 +1,42 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *  int val;
+ *  TreeNode * left;
+ *  TreeNode * right;
+ *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    
-    void solve(TreeNode* curr, int &count, int k,int &ans)
-    {
-        if(curr==NULL)
-            return ;
-        
-        solve(curr->left,count,k,ans);
-        count++;
-        if(count==k)
-        {
+class Solution
+{
+    public:
 
-        ans=curr->val;
-            return ;
+        void solve(TreeNode *curr, int &count, int k, int &ans)
+        {
+            if (curr == NULL)
+                return;
+
+            solve(curr->left, count, k, ans);
+           	// increase the value of count
+            count++;
+            if (count == k)	// this node is our ans
+            {
+
+                ans = curr->val;
+                return;
+            }
+            solve(curr->right, count, k, ans);
         }
-        solve(curr->right,count,k,ans);
-        
-    }
-    
-    int kthSmallest(TreeNode* root, int k) {
-      
-        int count=0, ans=0;
-        
-    solve(root,count,k,ans);
-        
+
+    int kthSmallest(TreeNode *root, int k)
+    {
+
+        int count = 0, ans = 0;
+       	// perform inorder traversal and increase the count variable 
+        solve(root, count, k, ans);
+
         return ans;
-        
     }
 };
