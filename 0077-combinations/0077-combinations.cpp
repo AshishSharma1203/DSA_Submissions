d23d51b67dfbc5ad7e842b03f054a9b38fe8503f
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-08-01 10:45:52 +0530

    Time: 403 ms (17.16%), Space: 164.1 MB (14.41%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,41 @@
+class Solution
+{
+    public:
+
+        void solve(int i, int n, int k, int count, vector<int> temp, vector<vector< int>> &ans)
+        {
+            if (count > k)
+                return;
+            if (count == k)
+            {
+                ans.push_back(temp);
+                return;
+            }
+
+            for (int j = i + 1; j <= n; j++)
+            {
+                temp.push_back(j);
+                solve(j, n, k, count + 1, temp, ans);
+                temp.pop_back();
+            }
+        }
+
+    vector<vector < int>> combine(int n, int k)
+    {
+
+        vector<int> temp;
+        vector<vector < int>> ans;
+
+        if (k > n)
+            return ans;
+
+        for (int i = 1; i <= n; i++)
+        {
+            temp.push_back(i);
+            solve(i, n, k, 1, temp, ans);
+            temp.pop_back();
+        }
+
+        return ans;
+    }
+};
