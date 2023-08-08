e8f51cc43c498ebb3a6a9e38dd6d8126f3490092
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-08-09 00:14:06 +0530

    Time: 908 ms (57.43%), Space: 0B (100.00%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -2,14 +2,11 @@
 
 # select num  from MyNumbers group by num , having count(num)=1  
 
-# select
-# max(num) as num 
-# from
-# (
-# select num from MyNumbers
-#     group by num having count(num)=1
-# ) as t ;
-
-
-SELECT (SELECT num FROM MyNumbers GROUP BY num HAVING COUNT(num) = 1 ORDER BY num DESC LIMIT 1) AS num;
+select
+max(num) as num 
+from
+(
+select num from MyNumbers
+    group by num having count(num)=1
+) as t ;
 
