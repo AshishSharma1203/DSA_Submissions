74e4835abf0bb5f1bd5c39da9a804de023b5b623
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-08-16 01:20:55 +0530

    Time: 8 ms (38.01%), Space: 10.3 MB (21.46%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.cpp
@@ -0,0 +1,74 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        
+        ListNode* ls=NULL,*le=NULL,*gs=NULL,*ge=NULL;
+        
+        for(ListNode* curr=head;curr!=NULL ;curr=curr->next)
+        {
+            
+            if(curr->val<x)
+            {
+                if(ls==NULL)
+                {
+                    ls=le=curr;
+                }
+                else 
+                {
+                    le->next=curr;
+                    le=le->next;
+                }
+            }
+            
+            else 
+            {
+
+                if(gs==NULL)
+                {
+                    gs=ge=curr;
+                    
+                }
+                else 
+                {
+                    ge->next=curr;
+                    ge=ge->next;
+                }
+            
+            }
+            
+        }
+        
+//         ge->next=NULL;
+        
+//         if(ls==NULL)
+//         {
+//             return gs;
+//         }
+        
+//         le->next=gs;
+//         return le;
+        if (ls == NULL) {
+    // If there are no elements less than x, return the list of elements greater than or equal to x
+    return gs;
+}
+
+le->next = gs;
+if (ge != NULL) {
+    // Make sure to terminate the list of elements greater than or equal to x
+    ge->next = NULL;
+}
+
+return ls;
+     
+    }
+};
