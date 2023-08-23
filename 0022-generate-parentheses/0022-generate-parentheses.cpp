7c497cb5d9c57019dd4c67cdff62bcc648915e81
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-08-23 10:33:36 +0530

    Time: 7 ms (29.37%), Space: 20 MB (7.92%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,55 @@
+class Solution
+{
+    public:
+
+        bool check(string s)
+        {
+
+            stack<char> st;
+
+            for (int i = 0; i < s.size(); i++)
+            {
+                if (s[i] == '(')
+                    st.push('(');
+
+                else
+                {
+                    if (st.empty())
+                        return false;
+                    else if (st.top() != '(')
+                        return false;
+
+                    else st.pop();
+                }
+            }
+
+            return st.empty() == true ? true : false;
+        }
+
+    void solve(int open, int close, string temp, vector<string> &ans)
+    {
+        if (open == 0 && close == 0)
+        {
+            if (check(temp))
+                ans.push_back(temp);
+            return;
+        }
+
+        if (open > 0)
+            solve(open - 1, close, temp + "(", ans);
+        if (close > open)
+            solve(open, close - 1, temp + ")", ans);
+    }
+
+    vector<string> generateParenthesis(int n)
+    {
+
+       	// int m = n;
+       	// stack < char>st;
+        vector<string> ans;
+        string temp;
+
+        solve(n, n, temp, ans);
+        return ans;
+    }
+};
