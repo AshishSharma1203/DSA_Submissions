c9801b2ee9b31406ebca80549d8b1e45bcd57ea1
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-09-01 12:02:54 +0530

    Time: 278 ms (31.48%), Space: 18.2 MB (5.01%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,17 +1,35 @@
-class Solution {
-public:
-    int jump(vector<int>& nums) {
-        int numOfJumps = 0, currentIntervalEnd = 0, farthestReachFoundSoFar = 0;
-        
-        for(int i = 0 ; i < nums.size() - 1 ; i++) {     
-            farthestReachFoundSoFar = max(farthestReachFoundSoFar, i + nums[i]);                                                                                                       
-                                                                                   
-            if(i == currentIntervalEnd) {              
-                numOfJumps++;                                 
-                currentIntervalEnd = farthestReachFoundSoFar;   
-                                                         
-            }                                             
-        }                                               
-        return numOfJumps;
+class Solution
+{
+    public:
+
+        int solve(int i, vector<int> &nums, vector<int> &dp)
+        {
+
+            if (i >= nums.size() - 1)
+                return 0;
+
+            if (dp[i] != -1)
+                return dp[i];
+
+            int ans = INT_MAX-1;
+
+            int max_steps = min(nums[i], (int) nums.size() - 1);
+
+            for (int j = 1; j <= max_steps; j++)
+            {
+
+                ans = min(ans, 1 + solve(i + j, nums, dp));
+            }
+
+            return dp[i] = ans;
+        }
+
+    int jump(vector<int> &nums)
+    {
+
+        int n = nums.size();
+        vector<int> dp(n, -1);
+
+        return solve(0, nums, dp);
     }
 };
