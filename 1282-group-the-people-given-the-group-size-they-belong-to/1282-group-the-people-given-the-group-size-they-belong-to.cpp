967249d2031a12f75b514f99bb19443e863b9020
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-09-11 23:45:05 +0530

    Time: 8 ms (78.38%), Space: 13.9 MB (17.00%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -4,69 +4,69 @@ class Solution
         vector<vector < int>> groupThePeople(vector<int> &groupSizes)
         {
 
-           	//             vector<vector < int>> ans;
-           	//             int n = groupSizes.size();
-           	//             vector<pair<int, int>> groups;
+           	            vector<vector < int>> ans;
+           	            int n = groupSizes.size();
+           	            vector<pair<int, int>> groups;
 
-           	//             for (int i = 0; i < n; i++)
-           	//             {
-           	//                 groups.push_back({ groupSizes[i],
-           	//                     i });
-           	//             }
-           	//             sort(groups.begin(), groups.end());
+           	            for (int i = 0; i < n; i++)
+           	            {
+           	                groups.push_back({ groupSizes[i],
+           	                    i });
+           	            }
+           	            sort(groups.begin(), groups.end());
 
-           	//             int i = 0;
-           	//             while (i < n)
-           	//             {
-           	//                 int count = groups[i].first;
-           	//                 vector<int> temp;
-           	//                 int j;
-           	//                 for (j = i; j < count; j++)
-           	//                 {
-           	//                     temp.push_back(groups[i].second);
-           	//                     count++;
-           	//                 }
-           	//                 ans.push_back(temp);
-           	//                 i = j;
-           	//             }
+           	            int i = 0;
+           	            while (i < n)
+           	            {
+           	                int count = groups[i].first;
+           	                vector<int> temp;
+           	                int j;
+           	                for (j = i; j < i+count; j++)
+           	                {
+           	                    temp.push_back(groups[j].second);
+           	                   
+           	                }
+           	                ans.push_back(temp);
+           	                i = j;
+           	            }
 
-           	//             return ans;
+           	            return ans;
 
-            int n = groupSizes.size();
+//             int n = groupSizes.size();
 
-            vector<vector < int>> ans;
-            map<int, vector < int>> mp;
-            for (int i = 0; i < n; i++)
-            {
+//             vector<vector < int>> ans;
+//             map<int, vector < int>> mp;
+//             for (int i = 0; i < n; i++)
+//             {
 
-                mp[groupSizes[i]].push_back(i);
-            }
+//                 mp[groupSizes[i]].push_back(i);
+//             }
 
-            for (auto i: mp)
-            {
+//             for (auto i: mp)
+//             {
 
-                int count = i.first;
-                vector<int> temp = i.second;
+//                 int count = i.first;
+//                 vector<int> temp = i.second;
 
-                int counter = 0;
-               	// int j = temp.size();
-                vector<int> groups;
+//                 int counter = 0;
+//                	// int j = temp.size();
+//                 vector<int> groups;
 
-                for (int k = 0; k < temp.size(); k++)
-                {
+//                 for (int k = 0; k < temp.size(); k++)
+//                 {
 
-                    groups.push_back(temp[k]);
-                    counter++;
+//                     groups.push_back(temp[k]);
+//                     counter++;
 
-                    if (counter == count)
-                    {
-                        ans.push_back(groups);
-                        counter = 0;
-                        groups.clear();
-                    }
-                }
-            }
+//                     if (counter == count)
+//                     {
+//                         ans.push_back(groups);
+//                         counter = 0;
+//                         groups.clear();
+//                     }
+//                 }
+//             }
 
-            return ans;
+//             return ans;
         }
 };
