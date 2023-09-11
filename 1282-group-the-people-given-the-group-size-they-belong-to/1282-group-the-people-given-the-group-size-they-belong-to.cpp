bb7ba3bbea5ca29d0c1539a65a265abc337b781c
Ashish <92261209+AshishSharma1203@users.noreply.github.com>
2023-09-11 23:36:27 +0530

    Time: 7 ms (87.01%), Space: 13.4 MB (31.66%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,72 @@
+class Solution
+{
+    public:
+        vector<vector < int>> groupThePeople(vector<int> &groupSizes)
+        {
+
+           	//             vector<vector < int>> ans;
+           	//             int n = groupSizes.size();
+           	//             vector<pair<int, int>> groups;
+
+           	//             for (int i = 0; i < n; i++)
+           	//             {
+           	//                 groups.push_back({ groupSizes[i],
+           	//                     i });
+           	//             }
+           	//             sort(groups.begin(), groups.end());
+
+           	//             int i = 0;
+           	//             while (i < n)
+           	//             {
+           	//                 int count = groups[i].first;
+           	//                 vector<int> temp;
+           	//                 int j;
+           	//                 for (j = i; j < count; j++)
+           	//                 {
+           	//                     temp.push_back(groups[i].second);
+           	//                     count++;
+           	//                 }
+           	//                 ans.push_back(temp);
+           	//                 i = j;
+           	//             }
+
+           	//             return ans;
+
+            int n = groupSizes.size();
+
+            vector<vector < int>> ans;
+            map<int, vector < int>> mp;
+            for (int i = 0; i < n; i++)
+            {
+
+                mp[groupSizes[i]].push_back(i);
+            }
+
+            for (auto i: mp)
+            {
+
+                int count = i.first;
+                vector<int> temp = i.second;
+
+                int counter = 0;
+               	// int j = temp.size();
+                vector<int> groups;
+
+                for (int k = 0; k < temp.size(); k++)
+                {
+
+                    groups.push_back(temp[k]);
+                    counter++;
+
+                    if (counter == count)
+                    {
+                        ans.push_back(groups);
+                        counter = 0;
+                        groups.clear();
+                    }
+                }
+            }
+
+            return ans;
+        }
+};
